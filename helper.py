fa52785ddbbfc31df42fbdd5a2762bd6b0a6a247
Daniel Nash <danielnash@usf.edu>
2023-10-30 17:17:00 -0400

    move getDate to helper module

--- helper.py
+++ helper.py
@@ -85,3 +85,39 @@ def createDatabase(databaseUser, databasePassword, databaseName, databaseHost, d
     with psycopg.connect(dbname=databaseName, user=databaseUser, password=databasePassword, host=databaseHost, port=databasePort) as connection:
         with connection.cursor() as cursor:
           cursor.execute(databaseQueryString)
+
+def getDate():
+    while True:
+        year = input('Enter year: ')
+        if year.isnumeric():
+            year = int(year)
+            if year <= 9999 and year >= 1:
+                break
+        print('Invalid input. Please input a valid year.')
+
+    while True:
+        month = input('Enter month: ')
+        if month.isnumeric():
+            month = int(month)
+            if month <= 12 and month >= 1:
+                break
+        print('Invalid input. Please input a valid month.')
+    
+    if month == 2:
+        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+            max = 29
+        else:
+            max = 28
+    elif month in (1, 3, 5, 7, 8, 10, 12):
+        max = 31
+    else: max = 30
+
+    while True:
+        day = input('Enter day: ')
+        if day.isnumeric():
+            day = int(day)
+            if day <= max and day >= 1:
+                break
+        print('Invalid input. Please input a valid day.')
+    
+    return f"{year}-{month}-{day}"
